6911facdada79b08f15e212d13859830cd6180e9
satohidetada <karatedamashi.123@gmail.comkarateshin.123@gmail.com>
2026-01-27 14:53:06 +0900

    fix: resolve build error and add seller profile info

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Usini - Official Site",
+  title: "Usini - HP",
   description: "群馬を拠点とするロックバンド「Usini」のホームページです。",
 };
 
