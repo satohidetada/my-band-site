7eddc806c7a66ed340e1455d3ea2b6d18bb1ec03
satohidetada <karatedamashi.123@gmail.comkarateshin.123@gmail.com>
2026-08-24 20:59:40 +0900

    Add Instagram link to Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -93,6 +93,8 @@ export default function ContactPage() {
           </p>
         </section>
       </main>
+    
+      <a href="https://www.instagram.com/ultrchnvr" target="_blank" rel="noopener noreferrer" className="mt-8 inline-block text-lime-400 hover:text-orange-400 underline transition duration-150">Instagram</a>
     </div>
   );
 }
